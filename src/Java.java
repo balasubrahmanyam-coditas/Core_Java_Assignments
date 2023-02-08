16ac7d35c497734bcfafdc197ea97ac0cc781d12
balasubrahmanyam-coditas <124357865+balasubrahmanyam-coditas@users.noreply.github.com>
2023-02-08 17:22:19 +0530

    Add files via upload

---
+++ src/Java.java
@@ -0,0 +1,73 @@
+Short
+class Short {
+    public static void main(String[] args) {
+        short a=1;
+        short b=32767;
+        short c=(short)(a+b);
+        System.out.println(c);
+    }
+}
+
+short+int=long
+class Short {
+    public static void main(String[] args) {
+        short a=32;
+        int b=2147483647;
+        long c=a+b;
+        System.out.println(c);
+    }
+}
+float+int=long
+class Int {
+    public static void main(String[] args) {
+        float a=0.3f;
+        int b=2147483647;
+        long c=(long)a+b;
+        System.out.println(c);
+    }
+}
+float+int=double
+class Int {
+    public static void main(String[] args) {
+        float a=0.3f;
+        int b=2147483647;
+        double c=a+b;
+        System.out.println(c);
+    }
+}
+short+int=double
+class Int {
+    public static void main(String[] args) {
+        short a=234;
+        int b=2147483647;
+        double c=a+b;
+        System.out.println(c);
+    }
+}
+double+long=long
+class Int {
+    public static void main(String[] args) {
+        double a=2.34d;
+        int b=2147;
+        long c=(long)a+b;
+        System.out.println(c);
+    }
+}
+byte+byte=long
+class Int {
+    public static void main(String[] args) {
+        byte a=2;
+        byte b=1;
+        long c=a+b;
+        System.out.println(c);
+    }
+}
+byte+byte=float
+class Int {
+    public static void main(String[] args) {
+        byte a=2;
+        byte b=1;
+        float c=(float)a+b;
+        System.out.println(c);
+    }
+}
