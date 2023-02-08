52c38e0b3c9e81a6ed52c51bb18c720493833e0f
balasubrahmanyam-coditas <124357865+balasubrahmanyam-coditas@users.noreply.github.com>
2023-02-08 17:23:12 +0530

    Add files via upload

---
+++ src/Simplecalculator.java
@@ -0,0 +1,30 @@
+class Simple{
+	int result;
+	int number1 = 45;
+	int number2 = 32;
+        public void add(){
+		result=number1+number2;
+		System.out.println("Addition is"+ result);
+	}
+	public void subtract(){
+		result=number1-number2;
+		System.out.println("Subtract is"+ result);
+	}
+	public void multiply(){
+		result=number1*number2;
+		System.out.println("Multiply is"+ result);
+	}
+	public void division(){
+		sresult=number1/number2;
+		System.out.println("Division is"+ result);
+	}
+}
+public class Simplecalculator{
+	public static void main(String args[]){
+	Simple calculator=new Simple();
+	calculator.add();
+	calculator.subtract();
+	calculator.multiply();
+	calculator.division();
+	}
+}
