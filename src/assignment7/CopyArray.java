2b070427989b553aea8d1043f4577273f80a197b
bala subrahmanyam <bala.subryanam@coditas.com>
2023-02-13 16:50:51 +0530

    assignment 7

--- src/assignment7/CopyArray.java
+++ src/assignment7/CopyArray.java
@@ -1,15 +1,18 @@
 package assignment7;
 
 class ArrayCopys{
-    int arr1[] = new int[] {2,3,3,3,2};
+    int arr1[] ={2,3,3,3,2};
     int arr2[] = new int[arr1.length];
     public void copy() {
         for (int i = 0; i < arr1.length; i++) {
             arr2[i] = arr1[i];
         }
         System.out.println("Elements of original array: ");
-        for (int i = 0; i < arr1.length; i++) {
+       /* for (int i = 0; i < arr1.length; i++) {
             System.out.print(arr1[i] + " ");
+        }*/
+        for(int i: arr1){
+            System.out.print(i+" ");
         }
         System.out.println();
         System.out.println("Elements of new array: ");
--- src/assignment7/DuplicateArray.java
+++ src/assignment7/DuplicateArray.java
@@ -1,30 +1,20 @@
 package assignment7;
-import java.util.*;
 
-class Array{
-    int arr[] = {2,3,2,3,4,5};
-    public void show(){
-        for(int i = 0; i < arr.length; i++) {
-            for(int j = i + 1; j < arr.length; j++) {
-                if(arr[i] == arr[j])
+class Array {
+    int arr[] = {2, 3, 2, 3, 4, 5};
+
+    public void show() {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j])
                     System.out.println(arr[j]);
             }
         }
-    }
-    public void remove(){
-        public void RemoveDuplicate(){
-            for (int i = 0, k = 0; i < arr.length; i++) {
-                if (i == arr[i]) {
-                    continue;
-                }
-                arr[k++] = arr[i];
-            }
     }
 }
 public class DuplicateArray {
     public static void main(String[] args) {
         Array array = new Array();
         array.show();
-        array.remove();
     }
 }
